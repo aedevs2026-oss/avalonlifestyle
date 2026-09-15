231e17c8c9cb6329ebaf29fda88d5c5e27593b0e
Aedevs <aedevs2026@gmail.com>
2026-09-15 19:52:42 +0530

    initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["pdf-parse"],
+  serverActions: {
+    bodySizeLimit: "4mb",
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
